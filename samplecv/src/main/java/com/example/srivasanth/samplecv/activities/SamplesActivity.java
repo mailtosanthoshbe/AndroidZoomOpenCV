1db09f42a1bb81801422400013ac7ec6e17202f2
Santhosh Kumar <mailtosanthoshbe@gmail.com>
2018-04-01 18:46:27 +0530

    initial setup

---
+++ samplecv/src/main/java/com/example/srivasanth/samplecv/activities/SamplesActivity.java
@@ -0,0 +1,64 @@
+package com.example.srivasanth.samplecv.activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.srivasanth.samplecv.R;
+
+public class SamplesActivity extends AppCompatActivity {
+
+    Intent sampleIntent;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_samples);
+    }
+
+    public void tutorial1(View v) {
+        sampleIntent = new Intent(this, Tutorial1Activity.class);
+        startActivity(sampleIntent);
+    }
+
+    public void tutorial2(View v) {
+        sampleIntent = new Intent(this, Tutorial2Activity.class);
+        startActivity(sampleIntent);
+    }
+
+    public void tutorial3(View v) {
+        sampleIntent = new Intent(this, Tutorial3Activity.class);
+        startActivity(sampleIntent);
+    }
+
+    public void imageManipulations(View v) {
+        sampleIntent = new Intent(this, ImageManipulationsActivity.class);
+        startActivity(sampleIntent);
+    }
+
+    public void faceDetection(View v) {
+        sampleIntent = new Intent(this, FaceDetectionActivity.class);
+        startActivity(sampleIntent);
+    }
+
+    public void colorBlobDetection(View v) {
+        sampleIntent = new Intent(this, ColorBlobDetectionActivity.class);
+        startActivity(sampleIntent);
+    }
+
+    public void cameraCalibration(View v) {
+        sampleIntent = new Intent(this, CameraCalibrationActivity.class);
+        startActivity(sampleIntent);
+    }
+
+    public void puzzle15(View v) {
+        sampleIntent = new Intent(this, Puzzle15Activity.class);
+        startActivity(sampleIntent);
+    }
+    public void onHandRecognition(View v) {
+        sampleIntent = new Intent(this, MainActivity.class);
+        startActivity(sampleIntent);
+    }
+
+}
---
+++ samplecv/src/main/java/com/example/srivasanth/samplecv/utils/calibration/PreviewFrameRender.java
@@ -0,0 +1,11 @@
+package com.example.srivasanth.samplecv.utils.calibration;
+
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.core.Mat;
+
+public class PreviewFrameRender extends FrameRender {
+    @Override
+    public Mat render(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        return inputFrame.rgba();
+    }
+}
